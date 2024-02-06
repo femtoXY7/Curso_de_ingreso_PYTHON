e1c1f8ad900ed37d518c239aefe1e7600a8463f5
Nacho <torresnacho14@gmail.com>
2024-02-06 09:56:50 -0300

    Resolvi ej 01

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -26,7 +26,7 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        pass
+        alert("hola tonto", "esto no anda, funka")
 
 
 if __name__ == "__main__":
